604b39a794138e4e6f85647986d8d5ca6cc51f44
Bernard Ograh <69661398+bograh@users.noreply.github.com>
2024-08-26 15:05:18 +0000

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://mini-project-cco8.onrender.com/api";
 
 export const api = {
     // User registration
